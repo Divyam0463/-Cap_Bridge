6eafb5e7b3fad077c3154d7c426babc998eefec7
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-02-05 10:49:46 +0530

    29-Jan...Day 3

---
+++ 29:01/Frequency.java
@@ -0,0 +1,12 @@
+public class Frequency {
+  static void freq(int[] arr) {
+    
+    }
+
+  }
+
+  public static void main(String[] args) {
+    int arr[] = { 11, 2, 2, 10, 9, 5, 9 };
+    freq(arr);
+  }
+}
