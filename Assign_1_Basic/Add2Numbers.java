6373961cbef724c71dc9f4a469d88fe44395e610
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-02-10 14:49:27 +0530

    Delete Assign_1_Basic directory

--- Assign_1_Basic/Add2Numbers.java
+++
@@ -1,7 +0,0 @@
-public class Add2Numbers {
-  public static void main(String[] args) {
-    int x= 10 ; 
-    int y =15 ; 
-    System.out.println(x+y);
-  }
-}
--- Assign_1_Basic/CelFah.java
+++
@@ -1,8 +0,0 @@
-class CelFah {
-  public static void main(String[] args) {
-    double cel = 38 ; 
-
-    double Fah = (cel * 9/5) + 32 ; 
-    System.out.println(Fah);
-  }
-}
