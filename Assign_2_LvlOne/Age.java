01c9a79ef5e23374487dbe8fcef0ac803e779b83
Divyam0463 <102290944+Divyam0463@users.noreply.github.com>
2025-02-11 15:54:46 +0530

    deleted folder

--- Assign_2_LvlOne/Age.java
+++
@@ -1,8 +0,0 @@
-class Age{
-  public static void main(String[] args) {
-    int birthyear  = 2000 ; 
-    int curryear = 2024 ; 
-
-    System.out.println("Harry's age : "+(curryear-birthyear));
-  }
-}
